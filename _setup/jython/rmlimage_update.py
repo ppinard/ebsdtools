138df765dd7d6249509053232d49e7d6a8bd2e38
Philippe Pinard <pinard.philippe@ireq.ca>
2009-05-05 10:33:07 -0400

    MOD Implementation of ftp download of rmlimage updates

--- _setup/jython/rmlimage_update.py
+++ _setup/jython/rmlimage_update.py
@@ -18,6 +18,7 @@ import os
 import glob
 import ConfigParser
 import shutil
+import tempfile
 
 # Third party modules.
 
@@ -54,6 +55,9 @@ class UpdateConfiguration:
       
       if config.has_option(location, "password"):
         self.password = config.get(location, "password")
+      
+      if config.has_option(location, "download_location"):
+        self.download_location = config.get(location, "download_location")
 
 def update(location):
   """
@@ -65,43 +69,54 @@ def update(location):
                                , location=location)
   
   if location == 'ftp':
-    pass
+    server = ftp.FTP(host = config.source
+                     , username = config.username
+                     , password = config.password)
+    
+    distros = server.directoryListing('distro/')['files'].keys()
+    files = []
+    for distro in distros:
+      if config.prefix in distro:
+        files.append(distro)
+    
+    lastVersionFilename = sorted(files)[-1]
+    server.download('distro/%s' % lastVersionFilename, config.download_location)
+    lastVersion = os.path.join(config.download_location, lastVersionFilename)
   elif location == 'local':
     files = glob.glob(os.path.join(config.source, '%s*.%s' % (config.prefix, config.filetype)))
     lastVersion = sorted(files)[-1]
-    
-    print lastVersion
-    zip = myzip.reader(filename=lastVersion, debug=True)
-    
-    #Remove and create source folder
-    rml_folder = os.path.join(config.jython_lib, 'rml/')
-    if os.path.exists(rml_folder): shutil.rmtree(rml_folder)
-    os.mkdir(rml_folder)
-    
-    #Extract zip
+  
+  zip = myzip.reader(filename=lastVersion, debug=True)
+  
+  #Remove and create source folder
+  rml_folder = os.path.join(config.jython_lib, 'rml/')
+  if os.path.exists(rml_folder): shutil.rmtree(rml_folder)
+  os.mkdir(rml_folder)
+  
+  #Extract zip
+  zip.extractAll(rml_folder)
+  zip.close()
+  
+  #Extract jar
+  jarFiles = glob.glob(os.path.join(rml_folder, '*.jar'))
+  for jarFile in jarFiles:
+    zip = myzip.reader(filename=jarFile, debug=True)
     zip.extractAll(rml_folder)
     zip.close()
-    
-    #Extract jar
-    jarFiles = glob.glob(os.path.join(rml_folder, '*.jar'))
-    for jarFile in jarFiles:
-      zip = myzip.reader(filename=jarFile, debug=True)
-      zip.extractAll(rml_folder)
-      zip.close()
-    
-    #Remove folders in library path
-    rmlimage_folder = os.path.join(config.jython_lib, 'rmlimage/')
-    if os.path.exists(rmlimage_folder): shutil.rmtree(rmlimage_folder)
-    
-    rmlshared_folder = os.path.join(config.jython_lib, 'rmlshared/')
-    if os.path.exists(rmlshared_folder): shutil.rmtree(rmlshared_folder)
-    
-    microscope_folder = os.path.join(config.jython_lib, 'microscope/')
-    if os.path.exists(microscope_folder): shutil.rmtree(microscope_folder)
-    
-    shutil.move(os.path.join(rml_folder, 'rmlimage/'), rmlimage_folder)
-    shutil.move(os.path.join(rml_folder, 'rmlshared/'), rmlshared_folder)
-    shutil.move(os.path.join(rml_folder, 'microscope/'), microscope_folder)
+  
+  #Remove folders in library path
+  rmlimage_folder = os.path.join(config.jython_lib, 'rmlimage/')
+  if os.path.exists(rmlimage_folder): shutil.rmtree(rmlimage_folder)
+  
+  rmlshared_folder = os.path.join(config.jython_lib, 'rmlshared/')
+  if os.path.exists(rmlshared_folder): shutil.rmtree(rmlshared_folder)
+  
+  microscope_folder = os.path.join(config.jython_lib, 'microscope/')
+  if os.path.exists(microscope_folder): shutil.rmtree(microscope_folder)
+  
+  shutil.move(os.path.join(rml_folder, 'rmlimage/'), rmlimage_folder)
+  shutil.move(os.path.join(rml_folder, 'rmlshared/'), rmlshared_folder)
+  shutil.move(os.path.join(rml_folder, 'microscope/'), microscope_folder)
 
 if __name__ == '__main__':
-  update('local')
+  update('ftp')
