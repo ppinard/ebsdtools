eb4842163f92bd48a53f9d4b2201b7a98836c421
Philippe Pinard <pinard.philippe@ireq.ca>
2009-06-12 12:50:30 -0400

    NEW function in pattern.py to apply a mask after the init of the Pattern object

--- indexation/pattern.py
+++ indexation/pattern.py
@@ -29,22 +29,22 @@ class PatternMap(rmlimage.kernel.ByteMap):
   def __init__(self, filepath=None, byteMap=None, maskMap=None):
     """
     PatternMap class (inherit :class:`ByteMap <rmlimage.kernel.ByteMap>`
-    
+
     Store the information related to an EBSD pattern.
-    
-    Two optional constructors: 
-    
+
+    Two optional constructors:
+
       - from a file (bitmap)
       - from a :class:`ByteMap <rmlimage.kernel.ByteMap>`
-    
+
     If a *maskMap* is given, the mask is apply to the pattern and to all subsequent calculations. For mask constructor, see :mod:`masks <EBSDTools.indexation.masks>`
-    
+
     :arg filepath: file path of a bitmap file
     :type filepath: str
-    
+
     :arg byteMap: byteMap of a pattern
     :type byteMap: :class:`ByteMap <rmlimage.kernel.ByteMap>`
-    
+
     :arg maskMap: binary map of a mask to be applied to the diffraction pattern before doing the Hough transform
     :type maskMap: :class:`BinMap <rmlimage.kernel.BinMap>`
     """
@@ -52,55 +52,67 @@ class PatternMap(rmlimage.kernel.ByteMap):
     if filepath != None and byteMap == None:
       file = java.io.File(filepath)
       map = IO.load(file)
-      
+
       if map.type == 'RGBMap':
         self.originalPattern = kernel.Transform.getBlueLayer(map)
       else:
         self.originalPattern = map
     elif filepath == None and byteMap != None:
       self.originalPattern = byteMap
-    
+
     assert self.originalPattern.type == 'ByteMap'
-    
+
+    rmlimage.kernel.ByteMap.__init__(self, self.originalPattern.width, self.originalPattern.height, self.originalPattern.pixArray)
+
     #Apply mask
-    self.maskMap = maskMap
-    if self.maskMap != None:
-      assert self.maskMap.getType() == 'BinMap'
-      assert self.originalPattern.size == self.maskMap.size
-      
-      patternMapMask = kernel.ByteMap(self.originalPattern.width, self.originalPattern.height)
-      MapMath.andOp(self.originalPattern, self.maskMap, patternMapMask)
-      
-      #Constructor
-      rmlimage.kernel.ByteMap.__init__(self, patternMapMask.width, patternMapMask.height, patternMapMask.pixArray)
-    else:
-      rmlimage.kernel.ByteMap.__init__(self, self.originalPattern.width, self.originalPattern.height, self.originalPattern.pixArray)
-  
+    self.maskMap = None
+    self.applyMask(maskMap)
+
+  def applyMask(self, maskMap=None):
+    """
+    Apply a mask on the original pattern
+
+    :arg maskMap: binary map of a mask to be applied to the diffraction pattern before doing the Hough transform
+    :type maskMap: :class:`BinMap <rmlimage.kernel.BinMap>`
+    """
+    if maskMap == None and self.maskMap == None: return
+
+    if maskMap != None:
+      self.maskMap = maskMap
+
+    assert self.maskMap.getType() == 'BinMap'
+    assert self.originalPattern.size == self.maskMap.size
+
+    patternMapMask = kernel.ByteMap(self.originalPattern.width, self.originalPattern.height)
+    MapMath.andOp(self.originalPattern, self.maskMap, patternMapMask)
+
+    self = patternMapMask
+
   def getMaskMap(self):
     """
-    Return the mask map used 
-    
+    Return the mask map used
+
     :rtype: :class:`BinMap <rmlimage.kernel.BinMap>` or ``None`` if no mask was applied
     """
-    
+
     return self.maskMap
-  
+
   def getOriginalPattern(self):
     """
     Return the original pattern before the mask was applied (if any)
-    
+
     :rtype: :class:`ByteMap <rmlimage.kernel.ByteMap>`
     """
-    
+
     return self.originalPattern
-  
+
   def getType(self):
     """
     Override the class type to show that *PatternMap* inherits a *ByteMap*
     """
-    
+
     return 'ByteMap'
-  
+
 if __name__ == '__main__':
   patt = PatternMap(filepath='testData/pattern1.bmp')
-  
+
--- indexation/test_pattern.py
+++ indexation/test_pattern.py
@@ -33,54 +33,58 @@ class TestPatternMap(unittest.TestCase):
 
   def setUp(self):
     unittest.TestCase.setUp(self)
-    
+
     self.basepath = os.path.join(EBSDTools.__path__[0], 'indexation/testData')
-    
+
     self.patt1 = pattern.PatternMap(filepath=os.path.join(self.basepath, 'pattern1.bmp'))
     self.patt2 = pattern.PatternMap(filepath=os.path.join(self.basepath, 'pattern2.jpg'))
-    
+
     file = java.io.File(os.path.join(self.basepath, 'pattern1.bmp'))
     byteMap = IO.load(file)
     self.patt1_bytemap = pattern.PatternMap(byteMap=byteMap)
-    
+
     self.maskMap = masks.MaskDisc(width=168, height=128, centroid=(84,64), radius=59)
     self.patt1_mask = pattern.PatternMap(filepath=os.path.join(self.basepath, 'pattern1.bmp'), maskMap=self.maskMap)
-    
+
   def tearDown(self):
     unittest.TestCase.tearDown(self)
-    
+
   def testSkeleton(self):
     #self.fail("Test if the TestCase is working.")
     self.assert_(True)
-  
-  def testInit(self):
+
+  def test__init__(self):
     self.assertEqual(self.patt1.width, 168)
     self.assertEqual(self.patt1.height, 128)
     self.assertEqual(self.patt1.getType(), 'ByteMap')
-    
+
     self.assertEqual(self.patt2.width, 168)
     self.assertEqual(self.patt2.height, 128)
     self.assertEqual(self.patt2.getType(), 'ByteMap')
-    
+
     self.assertEqual(self.patt1_bytemap.width, 168)
     self.assertEqual(self.patt1_bytemap.height, 128)
     self.assertEqual(self.patt1_bytemap.getType(), 'ByteMap')
-    
+
     self.assertEqual(self.patt1_mask.width, 168)
     self.assertEqual(self.patt1_mask.height, 128)
     self.assertEqual(self.patt1_mask.getType(), 'ByteMap')
-  
-  def testGetMaskMap(self):
+
+  def test_applyMask(self):
+    self.patt1.applyMask(self.maskMap)
+    self.assertEqual(self.patt1.pixArray, self.patt1_mask.pixArray)
+
+  def test_getMaskMap(self):
     self.assertEqual(self.patt1.getMaskMap(), None)
     self.assertEqual(self.patt2.getMaskMap(), None)
     self.assertEqual(self.patt1_bytemap.getMaskMap(), None)
     self.assertEqual(self.patt1_mask.getMaskMap().pixArray, self.maskMap.pixArray)
-  
-  def testGetOriginalPattern(self):
+
+  def test_getOriginalPattern(self):
     self.assertEqual(self.patt1.getOriginalPattern().pixArray, self.patt1.pixArray)
     self.assertEqual(self.patt2.getOriginalPattern().pixArray, self.patt2.pixArray)
     self.assertEqual(self.patt1_bytemap.getOriginalPattern().pixArray, self.patt1_bytemap.pixArray)
     self.assertEqual(self.patt1_mask.getOriginalPattern().pixArray, self.patt1.pixArray)
-  
+
 if __name__ == '__main__':
   unittest.main()
