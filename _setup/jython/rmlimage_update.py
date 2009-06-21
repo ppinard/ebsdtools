ba78f623f463dd749a83c24bf8f8612e62bf3f7f
ppinard <>
2009-06-20 20:22:16 -0400

    MOD Remove the unzip of all the jars since the classpath can be used instead MOD Extract the library files as well as the program

--- _setup/jython/rmlimage_update.py
+++ _setup/jython/rmlimage_update.py
@@ -24,6 +24,7 @@ import shutil
 # Local modules.
 import InternetTools.ftp.ftp as ftp
 import RandomUtilities.zipTools.myzip as myzip
+import DrixUtilities.Files as Files
 
 class UpdateConfiguration:
   def __init__(self
@@ -35,95 +36,120 @@ class UpdateConfiguration:
   def _readConfiguration(self, configurationFile, location):
     config = ConfigParser.SafeConfigParser()
     config.readfp(open(configurationFile, 'r'))
-    
+
     if config.has_section(location):
       if config.has_option(location, "source"):
         self.source = config.get(location, "source")
-      
-      if config.has_option(location, "jython_lib"):
-        self.jython_lib = config.get(location, "jython_lib")
-      
-      if config.has_option(location, "prefix"):
-        self.prefix = config.get(location, "prefix")
-        
+
+      if config.has_option(location, "path_lib"):
+        self.path_lib = config.get(location, "path_lib")
+
+      if config.has_option(location, "prefix_lib"):
+        self.prefix_lib = config.get(location, "prefix_lib")
+
+      if config.has_option(location, "path_gui"):
+        self.path_gui = config.get(location, "path_gui")
+
+      if config.has_option(location, "prefix_gui"):
+        self.prefix_gui = config.get(location, "prefix_gui")
+
       if config.has_option(location, "filetype"):
         self.filetype = config.get(location, "filetype")
-      
+
       if config.has_option(location, "username"):
         self.username = config.get(location, "username")
-      
+
       if config.has_option(location, "password"):
         self.password = config.get(location, "password")
-      
+
       if config.has_option(location, "download_location"):
         self.download_location = config.get(location, "download_location")
 
-def update(location):
+def updateJythonLib(location):
   """
   There are two types of location: ftp or local
   They are defined in the configuration file
   """
-  configurationFile = os.path.join(os.path.dirname(__file__), 'rmlimage_update.cfg')
+  configurationFile = Files.getCurrentModulePath(__file__, 'rmlimage_update.cfg')
   config = UpdateConfiguration(configurationFile=configurationFile
                                , location=location)
-  
+
   if location == 'ftp':
     server = ftp.FTP(host = config.source
                      , username = config.username
                      , password = config.password)
-    
+
     distros = server.directoryListing('distro/')['files'].keys()
     files = []
     for distro in distros:
-      if config.prefix in distro:
+      if config.prefix_lib in distro:
         files.append(distro)
-    
+
     lastVersionFilename = sorted(files)[-1]
     server.download('distro/%s' % lastVersionFilename, config.download_location)
     lastVersion = os.path.join(config.download_location, lastVersionFilename)
   elif location == 'local':
-    files = glob.glob(os.path.join(config.source, '%s*.%s' % (config.prefix, config.filetype)))
+    files = glob.glob(os.path.join(config.source, '%s*.%s' % (config.prefix_lib, config.filetype)))
     lastVersion = sorted(files)[-1]
-  
+
   #Read zip
   print lastVersion
   zip = myzip.reader(filename=lastVersion, debug=True)
-  
+
   #Remove and create source folder
-  rml_folder = os.path.join(config.jython_lib, 'rml/')
+  rml_folder = config.path_lib
   if os.path.exists(rml_folder): shutil.rmtree(rml_folder)
   os.mkdir(rml_folder)
-  
+
   #Extract zip
   zip.extractAll(rml_folder)
   zip.close()
-  
-  #Extract jar
-  jarFiles = glob.glob(os.path.join(rml_folder, '*.jar'))
-  for jarFile in jarFiles:
-    zip = myzip.reader(filename=jarFile, debug=True)
-    zip.extractAll(rml_folder)
-    zip.close()
-  
-  #Copy main jar to uniform jar name
+
+#  #Copy main jar to uniform jar name
   mainjar = glob.glob(os.path.join(rml_folder, 'rml-image_ebsd*.jar'))[0]
   print mainjar
-  shutil.copy(mainjar, os.path.join(rml_folder, 'rml-image_ebsd.jar'))
-  
-  #Remove folders in library path
-  rmlimage_folder = os.path.join(config.jython_lib, 'rmlimage/')
-  if os.path.exists(rmlimage_folder): shutil.rmtree(rmlimage_folder)
-  
-  rmlshared_folder = os.path.join(config.jython_lib, 'rmlshared/')
-  if os.path.exists(rmlshared_folder): shutil.rmtree(rmlshared_folder)
-  
-  microscope_folder = os.path.join(config.jython_lib, 'microscope/')
-  if os.path.exists(microscope_folder): shutil.rmtree(microscope_folder)
-  
-  shutil.move(os.path.join(rml_folder, 'rmlimage/'), rmlimage_folder)
-  shutil.move(os.path.join(rml_folder, 'rmlshared/'), rmlshared_folder)
-  shutil.move(os.path.join(rml_folder, 'microscope/'), microscope_folder)
+  os.rename(mainjar, os.path.join(rml_folder, 'rml-image_ebsd.jar'))
+
+def updateProgram(location):
+  """
+  There are two types of location: ftp or local
+  They are defined in the configuration file
+  """
+  configurationFile = Files.getCurrentModulePath(__file__, 'rmlimage_update.cfg')
+  config = UpdateConfiguration(configurationFile=configurationFile
+                               , location=location)
+
+  if location == 'ftp':
+    server = ftp.FTP(host = config.source
+                     , username = config.username
+                     , password = config.password)
+
+    distros = server.directoryListing('distro/')['files'].keys()
+    files = []
+    for distro in distros:
+      if config.prefix_gui in distro:
+        files.append(distro)
+
+    lastVersionFilename = sorted(files)[-1]
+    server.download('distro/%s' % lastVersionFilename, config.download_location)
+    lastVersion = os.path.join(config.download_location, lastVersionFilename)
+  elif location == 'local':
+    files = glob.glob(os.path.join(config.source, '%s*.%s' % (config.prefix_gui, config.filetype)))
+    lastVersion = sorted(files)[-1]
+
+  #Read zip
+  print lastVersion
+  zip = myzip.reader(filename=lastVersion, debug=True)
+
+  #Remove and create source folder
+  rml_folder = config.path_gui
+  if os.path.exists(rml_folder): shutil.rmtree(rml_folder)
+  os.mkdir(rml_folder)
+
+  #Extract zip
+  zip.extractAll(rml_folder)
+  zip.close()
 
 if __name__ == '__main__':
-  print __file__
-  update('ftp')
+  updateJythonLib('ftp')
+  updateProgram('ftp')
