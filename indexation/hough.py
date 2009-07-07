11a5a45c4b5dc00a866638319ff21f10128e2505
Philippe T. Pinard <ppinard@L1400>
2009-07-07 15:02:36 -0400

    MOD Update hough.py to bring in line with new class structure in rmlimage

--- indexation/hough.py
+++ indexation/hough.py
@@ -18,19 +18,12 @@ import os
 from math import pi
 
 # Third party modules.
-import rmlimage
 import rmlimage.io.IO as IO #To be removed
-import rmlimage.kernel as kernel
-import rmlimage.plugin.ebsd.Transform as Transform
-import rmlimage.plugin.ebsd.Threshold as Threshold
-import rmlimage.macro.python.cui.Analysis as Analysis
-import rmlimage.macro.command.cui.Filter as Filter
-import rmlimage.kernel.Contrast as Contrast
-import rmlshared.math.Stats as Stats
-import rmlimage.plugin.ebsd.Convolution as Convolution
-import rmlimage.kernel.Kernel as Kernel
-import rmlimage.kernel.MathMorph as MathMorph
-import rmlimage.kernel.Edit as Edit
+import rmlimage.core
+import rmlimage.module.ebsd as ebsd
+import rmlimage.macro.python.cui as macro
+
+import rmlshared.math as math
 
 # Local modules.
 import RandomUtilities.sort.sortDict as sortDict
@@ -39,118 +32,112 @@ import RandomUtilities.sort.sortDict as sortDict
 FINDPEAKS_TOP_HAT         = 'tophat'
 FINDPEAKS_BUTTERFLY       = 'butterfly'
 
-class HoughMap(rmlimage.plugin.ebsd.HoughMap):
+class HoughMap(ebsd.core.HoughMap):
   def __init__(self, patternMap, angleIncrement=0.5):
     """
     Create a Hough class (inherit :class:`HoughMap <rmlimage.plugin.ebsd.HoughMap>`
-    
-    Calculate the Hough Transform. 
+
+    Calculate the Hough Transform.
         A median and a contrast expansion is performed before doing the Hough transform.
-    
+MathMorph.median(houghMap_crop, 3)
     :arg patternMap: a pattern map
     :type patternMap: :class:`PatternMap <EBSDTools.indexation.pattern.PatternMap>`
-    
+
     :arg angleIncrement: angle increment (in deg)
     :type angleIncrement: float
     """
     self.patternMap = patternMap
-    
+
     #Calculate Hough
     patternHoughMap = self.patternMap.duplicate()
-    
-    Filter.median(patternHoughMap)
-    Contrast.expansion(patternHoughMap)
-    
-    houghMap = Transform().hough(patternHoughMap, angleIncrement*pi/180.0)
-    
-    rmlimage.plugin.ebsd.HoughMap.__init__(self
-                                           , houghMap.getRMax()
-                                           , houghMap.getDeltaR()
-                                           , houghMap.getDeltaTheta())
-    
-    for index in range(houghMap.size):
-      self.setPixValue(index, houghMap.getPixValue(index))
-    
+
+    rmlimage.core.MathMorph.median(patternHoughMap)
+    rmlimage.core.Contrast.expansion(patternHoughMap)
+
+    houghMap = ebsd.core.Transform().hough(patternHoughMap, angleIncrement*pi/180.0)
+
+    ebsd.core.HoughMap.__init__(self, houghMap)
+
   def findPeaks(self, method=FINDPEAKS_TOP_HAT):
     """
     Find the peaks in the Hough transform with the classic Top Hat thresholding
     """
-    
+
     if method == FINDPEAKS_TOP_HAT:
       self._findPeaks_top_hat()
     elif method == FINDPEAKS_BUTTERFLY:
       self._findPeaks_butterfly()
-  
+
   def _findPeaks_top_hat(self, **args):
     #Get peaks from Hough
-    peaksMap = EBSD.houghThresholding(self.houghMap)
-    identMap = Analysis.identify(peaksMap)
+    peaksMap = ebsd.core.Threshold.automaticTopHat(self.houghMap)
+    identMap = ebsd.core.Identification.identify(peaksMap)
     assert self.houghMap.size == identMap.size
-    
+
     #Get intensities of peaks from Hough Transform
     intensities = {}
     for objectId in range(0,identMap.getObjectCount()):
       intensities[objectId] = []
-    
+
     for index in range(identMap.size):
       objectId = identMap.pixArray[index]
       if objectId > 0:
         houghPixelValue = self.houghMap.getPixValue(index)
         intensities[objectId-1].append(houghPixelValue)
-    
+
     #Get centroid of peaks
     centroids = []
     centroidsXY = []
-    
-    xS, yS = Analysis.measureCentroid(identMap)
+
+    xS, yS = rmlimage.core.Analysis.getCentroid(identMap)
     for i in range(len(xS)):
       x = int(round(xS[i])); y = int(round(yS[i]))
       r = self.houghMap.getR(x, y)
       theta = self.houghMap.getTheta(x, y)
-      
+
       centroids.append((r,theta))
       centroidsXY.append((x,y))
-    
+
     #Get area of peaks
-    areas = Analysis.measureArea(identMap)
-    
+    areas = rmlimage.core.Analysis.getArea(identMap)
+
     #Store information in self._peaks
     self._peaks = {}
     for objectId in range(0,identMap.getObjectCount()):
-      averageIntensity = Stats.average(intensities[objectId])
-      stdDevIntensity = Stats.standardDeviation(intensities[objectId])
+      averageIntensity = math.Stats.average(intensities[objectId])
+      stdDevIntensity = math.Stats.standardDeviation(intensities[objectId])
       centroid = centroids[objectId]
       centroidXY = centroidsXY[objectId]
       area = areas[objectId]
-      
+
       peak = {'intensity': {'average': averageIntensity, 'standard deviation': stdDevIntensity}
               , 'centroid': centroid
               , 'centroidXY': centroidXY
               , 'area': area}
       self._peaks[objectId] = peak
-  
+
   def _findPeaks_butterfly(self):
     houghMap = self.duplicate()
-    
+
     #Crop white edges on top and bottom
     topY = houghMap.getY(self.patternMap.getMaskMap().getRadius())
     bottomY = houghMap.getY(-self.patternMap.getMaskMap().getRadius())
-    
+
     houghMap.setROI(0, topY, houghMap.width-1, bottomY)
-    houghMap_crop = Edit.crop(houghMap)
+    houghMap_crop = rmlimage.core.Edit.crop(houghMap)
     houghMap.resetROI()
-    
+
     #Apply median filter
-    MathMorph.median(houghMap_crop, 3)
-    
+    rmlimage.core.MathMorph.median(houghMap_crop, 3)
+
     houghMap_crop.setFile('hough1.bmp')
     IO.save(houghMap_crop)
-    
+
 #    MathMorph.closing(houghMap_crop, 1)
-    
+
     #3x3
 #    k = [[0,-2,0], [1,3,1], [0,-2,0]]
-    
+
     #9x9
     k = [[-10, -15, -22, -22, -22, -22, -22, -15, -10],
          [ -1,  -6, -13, -22, -22, -22, -13,  -6,  -1],
@@ -161,47 +148,36 @@ class HoughMap(rmlimage.plugin.ebsd.HoughMap):
          [  3,   6,   4,  -3, -22,  -3,   4,   6,   3],
          [ -1,  -6, -13, -22, -22, -22, -13,  -6,  -1],
          [-10, -15, -22, -22, -22, -22, -22, -15, -10]]
-    
-    kk = Kernel(k, 1)
-    
-    destMap = kernel.ByteMap(houghMap_crop.width, houghMap_crop.height)
-    Convolution.convolve(houghMap_crop, kk, destMap)
-    
+
+    kk = rmlimage.core.Kernel(k, 1)
+
+    destMap = rmlimage.core.ByteMap(houghMap_crop.width, houghMap_crop.height)
+    ebsd.core.Convolution.convolve(houghMap_crop, kk, destMap)
+
     destMap.setFile('hough1_b.bmp')
     IO.save(destMap)
-    
-    Convolution.convolve(houghMap_crop, kk)
-    
-    houghMap_crop.setFile('hough1_b.bmp')
-    IO.save(houghMap_crop)
-    
+
+#    Convolution.convolve(houghMap_crop, kk)
+#
+#    houghMap_crop.setFile('hough1_b.bmp')
+#    IO.save(houghMap_crop)
+
     #Thresholding
 #    peaksMap = Threshold.automaticTopHat(houghMap_crop)
-#    
+#
 #    #Clean peaks (small peaks)
-#    
-#    
+#
+#
 #    identMap = Analysis.identify(peaksMap)
 #    print identMap.getObjectCount()
-#    
+#
 #    peaksMap.setFile('peaks_b.bmp')
 #    IO.save(peaksMap)
-  
-  def getHoughMap(self):
-    """
-    Return the HoughMap object
-    
-    :rtype: :class:`HoughMap <rmlimage.plugin.ebsd.HoughMap>`
-    """
-    
-    if self.houghMap == None: self.calculateHough()
-    
-    return self.houghMap
-  
+
   def getPeaks(self):
     """
     Return the peaks dictionary containing:
-    
+
     ==========   ====================================================
     key          Description
     ==========   ====================================================
@@ -209,71 +185,71 @@ class HoughMap(rmlimage.plugin.ebsd.HoughMap):
     centroid     (:math:`\\rho`, :math:`\\theta`)
     area         in pixels
     ==========   ====================================================
-    
+
     :rtype: dict
     """
-    
+
     if self._peaks == None: self.findPeaks()
-    
+
     return self._peaks
-  
+
   def getPeaksCount(self):
     """
     Return the number of detected peaks
-    
+
     :rtype: int
     """
     if self._peaks == None: self.findPeaks()
-    
+
     return len(self._peaks)
-  
+
   def getPeakIntensity(self, peakId):
     """
     Return the average intensity of a peak
-    
+
     :arg peakId: id of the peak
     :type peakId: int
-    
+
     :rtype: float
     """
     if self._peaks == None: self.findPeaks()
-    
+
     return self._peaks[peakId]['intensity']['average']
-  
+
   def getPeakCentroid(self, peakId):
     """
     Return the centroid (:math:`\\rho`, :math:`\\theta`) of a peak
-    
+
     :arg peakId: id of the peak
     :type peakId: int
-    
+
     :rtype: tuple
     """
     if self._peaks == None: self.findPeaks()
-    
+
     return self._peaks[peakId]['centroid']
-  
+
   def getPeakArea(self, peakId):
     """
     Return the area of a peak in pixel
-    
+
     :arg peakId: id of the peak
     :type peakId: int
-    
+
     :rtype: float
     """
     if self._peaks == None: self.findPeaks()
-    
+
     return self._peaks[peakId]['area']
-  
+
 if __name__ == '__main__':
   import EBSDTools.indexation.masks as masks
   import EBSDTools.indexation.pattern as pattern
-  
+
   maskMap = masks.MaskDisc(width=168, height=128, centroid=(84,64), radius=59)
   P = pattern.PatternMap(filepath='testData/pattern1.bmp', maskMap=maskMap)
   H = HoughMap(P)
-  
+
   H.setFile('houghtt1.bmp')
   IO.save(H)
   H.findPeaks(method=FINDPEAKS_BUTTERFLY)
--- indexation/test_hough.py
+++ indexation/test_hough.py
@@ -33,65 +33,63 @@ class TestHough(unittest.TestCase):
 
   def setUp(self):
     unittest.TestCase.setUp(self)
-    
+
     self.basepath = os.path.join(EBSDTools.__path__[0], 'indexation/testData')
-    
-    pattern1 = pattern.Pattern(filepath=os.path.join(self.basepath, 'pattern1.bmp'))
-    self.hough1 = hough.Hough(pattern1)
-    
-    pattern2 = pattern.Pattern(filepath=os.path.join(self.basepath, 'pattern2.jpg'))
-    self.hough2 = hough.Hough(pattern2)
-    
-    maskMap = masks.createMaskDisc(width=168, height=128, centroid=(84,64), radius=59)
-    pattern1_mask = pattern.Pattern(filepath=os.path.join(self.basepath, 'pattern1.bmp'), maskMap=maskMap)
-    self.hough1_mask = hough.Hough(pattern1_mask)
-    
+
+    pattern1 = pattern.PatternMap(filepath=os.path.join(self.basepath, 'pattern1.bmp'))
+    self.hough1 = hough.HoughMap(pattern1, angleIncrement=0.5)
+    self.hough1_angle2 = hough.HoughMap(pattern1, angleIncrement=2)
+
+    pattern2 = pattern.PatternMap(filepath=os.path.join(self.basepath, 'pattern2.jpg'))
+    self.hough2 = hough.HoughMap(pattern2)
+
+    maskMap = masks.MaskDisc(width=168, height=128, centroid=(84,64), radius=59)
+    pattern1_mask = pattern.PatternMap(filepath=os.path.join(self.basepath, 'pattern1.bmp'), maskMap=maskMap)
+    self.hough1_mask = hough.HoughMap(pattern1_mask)
+
   def tearDown(self):
     unittest.TestCase.tearDown(self)
-    
+
   def testSkeleton(self):
     #self.fail("Test if the TestCase is working.")
     self.assert_(True)
-  
+
   def testInit(self):
     #Byte map (bmp)
-    self.assertEqual(self.hough1.pattern.width, 168)
-    self.assertEqual(self.hough1.pattern.height, 128)
-    
+    self.assertEqual(self.hough1.patternMap.width, 168)
+    self.assertEqual(self.hough1.patternMap.height, 128)
+
     #RGB Map (jpg)
-    self.assertEqual(self.hough2.pattern.width, 168)
-    self.assertEqual(self.hough2.pattern.height, 128)
-  
+    self.assertEqual(self.hough2.patternMap.width, 168)
+    self.assertEqual(self.hough2.patternMap.height, 128)
+
   def testCalculateHough(self):
     #Default
-    self.hough1.calculateHough(angleIncrement=0.5)
-    houghMap = self.hough1.getHoughMap()
+    houghMap = self.hough1
     houghMap.setFile(os.path.join(self.basepath, 'test_hough1.bmp'))
     IO.save(houghMap)
     self.assert_(testImage.equal(os.path.join(self.basepath, 'test_hough1.bmp'), os.path.join(self.basepath, 'hough1.bmp')))
-    
+
     #Different angle increment
-    self.hough1.calculateHough(angleIncrement=2)
-    houghMap = self.hough1.getHoughMap()
+    houghMap = self.hough1_angle2
     houghMap.setFile(os.path.join(self.basepath, 'test_hough2.bmp'))
     IO.save(houghMap)
     self.assert_(testImage.equal(os.path.join(self.basepath, 'test_hough2.bmp'), os.path.join(self.basepath, 'hough2.bmp')))
-    
+
     #With mask
-    self.hough1_mask.calculateHough(angleIncrement=0.5)
-    houghMap = self.hough1_mask.getHoughMap()
+    houghMap = self.hough1_mask
     houghMap.setFile(os.path.join(self.basepath, 'test_hough3.bmp'))
     IO.save(houghMap)
     self.assert_(testImage.equal(os.path.join(self.basepath, 'test_hough3.bmp'), os.path.join(self.basepath, 'hough3.bmp')))
-  
+
   def testFindPeaks(self):
 #TODO: testFindPeaks in test_hough.py
     pass
-  
+
   def testCalculateImageQuality(self):
 #TODO: testCalculateImageQuality in test_hough.py
     pass
-  
+
 def cleanup():
   files = os.listdir(os.path.join(EBSDTools.__path__[0], 'indexation/testData/'))
 
@@ -103,4 +101,5 @@ if __name__ == '__main__':
   try:
     unittest.main()
   finally:
-    cleanup()
+    pass
+#    cleanup()
