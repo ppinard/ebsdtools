89397ff4395f015ac0cd891889202fa47c881cb0
cf0548 <cf0548@PCS00171>
2009-08-12 13:33:17 -0400

    Added MaskDiscFactory to create a MaskDisc usable by RML-Image

---
+++ indexation/MaskDiscFactory.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env jython
+""" """
+
+# Script information for the file.
+__author__ = "Philippe Pinard (philippe.pinard@gmail.com)"
+__version__ = ""
+__date__ = ""
+__copyright__ = "Copyright (c) 2009 Philippe Pinard"
+__license__ = ""
+
+# Subversion informations for the file.
+__svnRevision__ = ""
+__svnDate__ = ""
+__svnId__ = ""
+
+# Standard library modules.
+
+# Third party modules.
+
+# Local modules.
+import java
+
+
+# Globals and constants variables.
+class MaskDiskFactory(java.lang.Object, rmlimage.module.ebsd.python.interfaces):
+  
+  def create(self, width, height, centroidX, centroidY, radius):
+    """
+    Create a circular mask for a pattern size of *size* centered at *centroid* 
+    with a given *radius*
+    
+    :arg size: dimensions of the mask (width, height)
+    :type size: tuple
+    
+    :arg centroid: position in pixels of the center of the disc (x,y)
+    :type centroid: tuple
+    
+    :arg radius: radius of the disc in pixels
+    :type radius: int
+    
+    :rtype: :class:`BinMap <rmlimage.kernel.BinMap>`
+    """
+    
+    return MaskDisc(width, height, centroidX, centroidY, radius)
+    
